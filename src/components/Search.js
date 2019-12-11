abab53fdd6a06ba004264b14ae657f578f7b5f93
Floriane <floriane@tuta.io>
2019-12-11 14:02:02 -0500

    Displays search results preview

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
-import { Input, Grid } from "semantic-ui-react";
+import {
+  Input,
+  Grid,
+  Message,
+  List,
+  Image,
+  Segment,
+  Container
+} from "semantic-ui-react";
 import AuthService from "../utils/AuthService";
 
-const fetchArtists = async (query, limit) => {
+const fetchArtists = async (query, limit = 20) => {
   const url = new URL(`${process.env.REACT_APP_SPOTIFY_API}/search`);
   const params = {
     q: query,
     type: "artist",
-    limit: limit,
-    client_id: process.env.REACT_APP_SPOTIFY_ID
+    limit
   };
   Object.entries(params).forEach(([key, value]) =>
     url.searchParams.append(key, value)
@@ -23,27 +30,70 @@ const fetchArtists = async (query, limit) => {
   return data;
 };
 
+const formatArtist = ({ images, name }, index) => ({
+  name,
+  image: images[0] && images[0].url
+});
+
 const Search = () => {
   const [search, setSearch] = useState("");
+  const [artists, setArtists] = useState([]);
+  const [status, setStatus] = useState(null);
 
   const handleSearch = e => {
     if (e.key === "Enter") {
-      fetchArtists(search);
+      fetchArtists(search).then(handleResponse);
     } else {
       const value = e.target.value;
       setSearch(value);
-      fetchArtists(value, 10).then(data => console.log(data));
+      value ? fetchArtists(value, 5).then(handleResponse) : setArtists([]);
+    }
+  };
+
+  const handleResponse = ({ error, artists }) => {
+    setStatus(null);
+    setArtists([]);
+    if (error) {
+      return setStatus({ state: "error", message: error.message });
+    }
+    if (artists) {
+      setArtists(artists.items.map(formatArtist));
     }
   };
 
   return (
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
+        {status && status.state === "error" && (
+          <Message
+            error
+            header="We're sorry, an error occured"
+            content={status.message}
+          />
+        )}
         <Input
-          onKeyUp={handleSearch}
-          icon="search"
           placeholder="Search for an artist..."
+          icon="search"
+          onChange={handleSearch}
+          value={search}
+          size="big"
         />
+        {artists && !!artists.length && (
+          <Container className="results">
+            <Segment>
+              <List divided selection verticalAlign="middle">
+                {artists.map(({ name, image }, index) => (
+                  <List.Item key={`${name}-${index}`}>
+                    <Image avatar src={image} />
+                    <List.Content>
+                      <List.Header>{name}</List.Header>
+                    </List.Content>
+                  </List.Item>
+                ))}
+              </List>
+            </Segment>
+          </Container>
+        )}
       </Grid.Column>
     </Grid>
   );
