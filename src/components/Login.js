1f476fc335b997099b8e4315aafb1b45fb042f00
Floriane <floriane@tuta.io>
2019-12-11 11:31:10 -0500

    Handles authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Grid } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
+import AuthService from "../utils/AuthService";
+import { useLocation } from "react-router-dom";
+import { Message } from "semantic-ui-react";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const location = useLocation();
+
+  if (AuthService.isLoggedIn()) {
+    return <Redirect to="/search" />;
+  }
+
+  const query = location.hash.substr(1) || location.search.substr(1);
+  if (query) {
+    const params = new URLSearchParams(query);
+    if (params.has("error")) {
+      setError(params.get("error"));
+      location.search = "";
+    } else {
+      AuthService.setSession(params);
+      return <Redirect to="/search" />;
+    }
+  }
+
   const logUser = () => {
     const url = new URL(process.env.REACT_APP_SPOTIFY_ACCOUNT_ENDPOINT);
     const params = {
       client_id: process.env.REACT_APP_SPOTIFY_ID,
       response_type: "token",
-      redirect_uri: "http://localhost:3000/search"
+      redirect_uri: "http://localhost:3000/login"
     };
 
     Object.entries(params).forEach(([key, value]) =>
@@ -19,11 +42,18 @@ const Login = () => {
   return (
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
+        {error && (
+          <Message
+            error
+            header="We're sorry, an error occured"
+            content={error}
+          />
+        )}
         <Button
           href={logUser()}
           as="a"
           content="Login with Spotify"
-          icon={{ name: "spotify", color: "white" }}
+          icon={{ name: "spotify" }}
           labelPosition="right"
           size="huge"
           color="green"
--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -1,5 +1,18 @@
-const AuthService = {
-  isLoggedIn: () => true
-};
+function setSession(params) {
+  const expiresAt = JSON.stringify(
+    params.get("expires_in") * 1000 + new Date().getTime()
+  );
+  localStorage.setItem("access_token", params.get("access_token"));
+  localStorage.setItem("expires_at", expiresAt);
+}
 
-export default AuthService;
+function getToken() {
+  return localStorage.getItem("access_token");
+}
+
+function isLoggedIn() {
+  let expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+  return new Date().getTime() < expiresAt;
+}
+
+export default { isLoggedIn, setSession, getToken };
