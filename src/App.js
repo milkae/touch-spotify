f73deffd91b11d8629785dc9fa0ceba4cf567380
Floriane <floriane@tuta.io>
2019-12-10 21:26:13 -0500

    Separates private routes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,37 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect
 } from "react-router-dom";
-import Login from './components/Login'
+import Login from "./components/Login";
+import Search from "./components/Search";
+import AuthService from "./utils/AuthService";
 
 export default function App() {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Redirect from="/" to="/search" />
+          <PrivateRoutes />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
+const PrivateRoutes = () => {
+  let isLoggedIn = AuthService.isLoggedIn();
+  return !isLoggedIn ? (
+    <Redirect to="/login" />
+  ) : (
+    <Switch>
+      <Route path="/search">
+        <Search />
+      </Route>
+    </Switch>
+  );
+};
